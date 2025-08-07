75b50a3064495d25075e60e66137f296fe8744f3
fxh803 <2210904816@qq.com>
2025-08-07 14:41:54 +0800

    fix bug

--- src/stores/collageSeries.ts
+++ src/stores/collageSeries.ts
@@ -17,7 +17,7 @@ export const useCollageSeriesStore = defineStore('collageSeries', () => {
     }
 
     // 初始化一个空白幻灯片
-    function initializeEmptySlide() { 
+    function initializeEmptySlide() {
         const canvasInstance = canvasRef.value?.()
         if (!canvasInstance) return
 
@@ -31,7 +31,7 @@ export const useCollageSeriesStore = defineStore('collageSeries', () => {
     }
 
     // 更新当前幻灯片
-    function updateCurrentSlide() { 
+    function updateCurrentSlide() {
         if (stopListen.value) return // 如果是幻灯片操作，不更新
 
         const canvasInstance = canvasRef.value?.()
@@ -59,29 +59,29 @@ export const useCollageSeriesStore = defineStore('collageSeries', () => {
         })
         //遍历jsonObj，将他们的dataType做成一个数组
         const dataTypeArray = canvasInstance.getObjects().map((obj: any) => obj.get('dataType'))
-        console.log("debug:",dataTypeArray)
+        console.log("debug:", dataTypeArray)
         collageSeries.value[currentSlideIndex.value] = { json, preview, dataTypeArray }
     }
 
     // 清空画布
-    function clearCanvas() { 
+    function clearCanvas() {
         const canvasInstance = canvasRef.value?.()
         if (!canvasInstance) return
 
-        const objects = canvasInstance.getObjects().concat() 
-        if(objects.length > 0){
+        const objects = canvasInstance.getObjects().concat()
+        if (objects.length > 0) {
             objects.forEach(obj => canvasInstance.remove(obj))
         }
     }
 
     // 添加新幻灯片
-    function addNewSlide() { 
+    function addNewSlide() {
         const canvasInstance = canvasRef.value?.()
         if (!canvasInstance) return
         stopListen.value = true
         // 清空画布
         clearCanvas()
-        
+
         // 创建新的空白幻灯片
         const json = JSON.stringify(canvasInstance.toJSON())
         const preview = canvasInstance.toDataURL({
@@ -90,11 +90,12 @@ export const useCollageSeriesStore = defineStore('collageSeries', () => {
         })
         collageSeries.value.push({ json, preview, dataTypeArray: [] })
         currentSlideIndex.value = collageSeries.value.length - 1
-        stopListen.value = false 
+        stopListen.value = false
+        overviewStore.updateMarkerObjects()
     }
 
     // 选择幻灯片
-    function handleCollageSeriesSelect(idx: number) { 
+    function handleCollageSeriesSelect(idx: number) {
         const canvasInstance = canvasRef.value?.()
         if (!canvasInstance || !collageSeries.value[idx]) return
         stopListen.value = true
@@ -102,44 +103,44 @@ export const useCollageSeriesStore = defineStore('collageSeries', () => {
         const json = collageSeries.value[idx].json
         const jsonObj = typeof json === 'string' ? JSON.parse(json) : json
         const dataTypeArray = collageSeries.value[idx].dataTypeArray
-     
+
         // 清空当前画布
-        clearCanvas() 
-        if(jsonObj.objects.length > 0){
+        clearCanvas()
+        if (jsonObj.objects.length > 0) {
             canvasInstance.loadFromJSON(json, () => {
                 setTimeout(() => {
-                    if (canvasInstance) {
-                        canvasInstance.renderAll()
-                        // 恢复自定义属性
-                        restoreCustomProperties(canvasInstance, dataTypeArray) 
-                        overviewStore.updateMarkerObjects()
-                    }
+                    canvasInstance.renderAll()
+                    // 恢复自定义属性
+                    restoreCustomProperties(canvasInstance, dataTypeArray)
                     stopListen.value = false
+                    overviewStore.updateMarkerObjects()
                 }, 200)
             })
-        }else{
+        } else {
             stopListen.value = false
+            overviewStore.updateMarkerObjects()
         }
-        
+
     }
 
     // 删除幻灯片
-    function handleDeleteCollageSeries(idx: number) { 
+    function handleDeleteCollageSeries(idx: number) {
         if (collageSeries.value.length <= 1) return // 至少保留一个幻灯片
 
         // 如果删除的是当前幻灯片 
         if (idx === currentSlideIndex.value) {
             currentSlideIndex.value = Math.max(0, idx - 1)
             handleCollageSeriesSelect(currentSlideIndex.value)
-        } else if(idx < currentSlideIndex.value){
+        } else if (idx < currentSlideIndex.value) {
             currentSlideIndex.value -= 1
         }
-        collageSeries.value.splice(idx, 1) 
+        collageSeries.value.splice(idx, 1)
+        overviewStore.updateMarkerObjects()
     }
 
     // 恢复自定义属性
     function restoreCustomProperties(canvasInstance: Canvas, dataTypeArray: any) {
-        const objects = canvasInstance.getObjects() 
+        const objects = canvasInstance.getObjects()
         // 遍历画布对象和JSON对象，恢复自定义属性
         objects.forEach((obj, index) => {
             if (dataTypeArray[index]) {
@@ -148,7 +149,7 @@ export const useCollageSeriesStore = defineStore('collageSeries', () => {
         })
     }
 
-    
+
 
     return {
         collageSeries,
